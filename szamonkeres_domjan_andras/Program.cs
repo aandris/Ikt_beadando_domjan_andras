636607ba0c5ce40f998169687e15b69de9b01ab4
aandr1s <b3domand@vasvari.org>
2025-10-19 18:02:24 +0200

    adatok bekerese

--- szamonkeres_domjan_andras/Program.cs
+++ szamonkeres_domjan_andras/Program.cs
@@ -10,7 +10,29 @@ namespace szamonkeres_domjan_andras
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Kérem adja meg a vezetéknevét: ");
+            string vezeteknev = Console.ReadLine();
+
+            Console.WriteLine("Kérem adja meg a keresztnevét: ");
+            string keresztnev = Console.ReadLine();
+
+            string nev = vezeteknev + " " + keresztnev;
+
+            double testsuly;
+
+            while (true)
+            {
+                Console.WriteLine("Kérem adja meg a testsúlyát kilogrammban: ");
+                if (double.TryParse(Console.ReadLine(), out testsuly) && testsuly > 50 && testsuly <= 120)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Hibás adat! A testsúly 50 és 120 között kell legyen.");
+                }
+
+            }
     }
+}
 }
